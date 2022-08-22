c96fa259b9a30a8379a327637637262d02830655
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-22 21:12:13 +0300

    Removed the h6 markup

--- src/App.tsx
+++ src/App.tsx
@@ -60,12 +60,6 @@ function App() {
           </li>
         </ul>
 
-        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-          <span>Saved reports</span>
-          <a className="link-secondary" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle" className="align-text-bottom"></span>
-          </a>
-        </h6>
       </div>
     </nav>
 
